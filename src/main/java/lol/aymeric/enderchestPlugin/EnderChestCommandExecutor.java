cf7a45c7be936527ec07e1eace5581642c15047f
Aymeric <aymericver@icloud.com>
2025-02-26 17:08:47 +0100

    1.1 Update
    Added simple chat messages which add a chat message when command is used.

--- src/main/java/lol/aymeric/enderchestPlugin/EnderChestCommandExecutor.java
+++ src/main/java/lol/aymeric/enderchestPlugin/EnderChestCommandExecutor.java
@@ -1,5 +1,8 @@
 package lol.aymeric.enderchestPlugin;
 
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,8 +25,23 @@ public class EnderChestCommandExecutor implements CommandExecutor {
 
         Player player = (Player) sender;
 
-        Inventory enderChestInventory = player.getEnderChest();
-        player.openInventory(enderChestInventory);
-        return true;
+        try {
+            Inventory enderChestInventory = player.getEnderChest();
+            player.openInventory(enderChestInventory);
+
+            TextComponent successMessage = new TextComponent("Successfully opened Ender Chest, ");
+            successMessage.setBold(true);
+
+            TextComponent playerName = new TextComponent(player.getName());
+
+            successMessage.addExtra(playerName);
+            player.spigot().sendMessage(successMessage);
+            return true;
+        } catch (Exception e) {
+            TextComponent failureMessage = new TextComponent("Failed to open Ender Chest.");
+            failureMessage.setBold(true);
+            player.spigot().sendMessage(failureMessage);
+            return false;
+        }
     }
 }
